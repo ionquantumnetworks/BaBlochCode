8ea631f81aa157f6f6f1beb1faa50c157b6b1174
James Siverns <jsiverns@umd.edu>
2019-06-03 15:25:23 -0400

    added 4th level to optical pumping code. Changed units to [s] and [Hz].

--- barium3opticalpumping.py
+++ barium3opticalpumping.py
@@ -10,50 +10,75 @@ import scipy.constants as sc
 import numpy as np
 from qutip import *
 
+#Units
+MHz = 1*10**6
+kHz = 1*10**3
+ns = 1*10**-9
+us = 1*10**-6
 
+#Laser Detunings -2*sc.pi*
+Deltag = 2*sc.pi*0*MHz #detuning of 493 laser
+Deltar = 2*sc.pi*0*MHz#detuning of 650 laser
+DeltaQ= 2*sc.pi*0 #detuning of 1762 laser
 
-#Units of frequnecies are in MZ
-#Time units will be in us
-Deltag = -2*sc.pi*10 #detuning of 493 laser
-Deltar = 2*sc.pi*25 #detuning of 650 laser
-Om12 = 2*sc.pi*50 #Rabi frequrency of |1> to |2> 
-Om23 = 2*sc.pi*0 #Rabi frequrency of |2> to |3>
-gamma21 =  2*sc.pi*15.1 #Decay rate of |2> to |1>
-gamma23 =  2*sc.pi*5.3 #Decay rate of |2> to |3>
-gammalg = 2*sc.pi*2 #493 laser linewidth
-gammalr = 2*sc.pi*2 #650 laser linewidth
-tlist = np.linspace(0, 2, 1000) #List of points for plotting purposes
+#Rabi Frequnecies
+Om12 = 2*sc.pi*0*MHz #Rabi frequency of |1> to |2> 
+Om23 = 2*sc.pi*0*MHz #Rabi frequency of |2> to |3>
+Om14 = 2*sc.pi*1*MHz #Rabi frequency of |1> to |4>
 
-#Basis states for three levels
-one, two, three = qutrit_basis()
+#Decay rates
+gamma21 =  0#2*sc.pi*15.1*MHz #Decay rate of |2> to |1>
+gamma23 =  0#2*sc.pi*5.3*MHz #Decay rate of |2> to |3>
+gamma41 =  2*sc.pi*5.1*10**-3 #Decay rate of |4> to |1> used a lifetime of 31.2 s
+
+#Laser Linewidths
+gammalg = 0#2*sc.pi*2*MHz #493 laser linewidth
+gammalr = 0#2*sc.pi*2*MHz #650 laser linewidth
+gammalQ = 2*sc.pi*100*kHz #1762 laser linewidth
+
+tlist = np.linspace(0, 10*us, 1000) #List of points for plotting purposes
 
 #Operators between |n> and |m> 
-sig11 = one * one.dag()
-sig22 = two * two.dag()
-sig33 = three * three.dag()
-sig12 = one * two.dag()
-sig32 = three * two.dag()
-sig21 = two * one.dag()
-sig23 = two * three.dag()
-sig13 = one * three.dag()
-e_ops = [sig33] #operators to input into mesolve
+sig11 = basis(4,0) * basis(4,0).dag()
+sig12 = basis(4,0) * basis(4,1).dag()
+sig13 = basis(4,0) * basis(4,2).dag()
+sig14 = basis(4,0) * basis(4,3).dag()
+sig21 = basis(4,1) * basis(4,0).dag()
+sig22 = basis(4,1) * basis(4,1).dag()
+sig23 = basis(4,1) * basis(4,2).dag()
+sig24 = basis(4,1) * basis(4,3).dag()
+sig31 = basis(4,2) * basis(4,0).dag()
+sig32 = basis(4,2) * basis(4,1).dag()
+sig33 = basis(4,2) * basis(4,2).dag()
+sig34 = basis(4,2) * basis(4,3).dag()
+sig41 = basis(4,3) * basis(4,0).dag()
+sig42 = basis(4,3) * basis(4,1).dag()
+sig43 = basis(4,3) * basis(4,2).dag()
+sig44 = basis(4,3) * basis(4,3).dag()
+
+#operators to input into mesolve
+e_ops = [sig11,sig22,sig33,sig44]
 
 #Hamiltonian of system RWA
-H = (Deltag*sig11 + Deltar*sig33 + 0.5*Om12*sig12 + 0.5*Om12*sig21 + 0.5*Om23*sig23 + 0.5*Om23*sig32)
-#Initial state of system
-psi0 = one
-#Decays and dissipations
+H = (Deltag*sig11 + 0.5*Om12*sig12 + 0.5*Om14*sig14 + 0.5*Om12*sig21 + 0.5*Om23*sig23 + 0.5*Om23*sig32 + Deltar*sig33 + 0.5*Om14*sig41 + DeltaQ*sig44)
+
+##Initial state of system  
+psi0 = basis(4,0)
+
+##Decays and dissipations
 C21 = np.sqrt(gamma21) * sig12 #Decay from |2> to |1>
 C23 = np.sqrt(gamma23) * sig32 #Decay from |2> to |3>
+C41 = np.sqrt(gamma41) * sig14 #Decay from |4> to |1>
 Clg = np.sqrt(2*gammalg) * sig11 #From 493 laser linewidth
 Clr = np.sqrt(2*gammalr) * sig33 #From 650 laser linewidth
-c_ops = [C21,C23,Clg,Clr]
-
-#Solutions
+ClQ = np.sqrt(2*gammalQ) * sig44 #From 1762 laser linewidth
+c_ops = [C21,C23,C41,Clg,Clr,ClQ]
+#
+##Solutions
 n = mesolve(H, psi0, tlist, c_ops, e_ops) #Solves Hamiltionian at point on tlist
-final_state = steadystate(H, c_ops) #Solve Hamiltonian for t = infinity
-fexpt = expect(e_ops, final_state) #Calculates expectation values (e_ops list) for Hamiltonion at t = inifinity
-#print(sig23)
+#final_state = steadystate(H, c_ops) #Solve Hamiltonian for t = infinity
+#fexpt = expect(e_ops, final_state) #Calculates expectation values (e_ops list) for Hamiltonion at t = inifinity
+print(H)
 plot_expectation_values(n, show_legend=True,figsize=(8, 8))
 
 #Save graph data
