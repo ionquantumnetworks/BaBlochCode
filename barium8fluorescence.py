8ba97288f9b5977085ca6b67382d50e59f8fa7c3
James Siverns <jsiverns@umd.edu>
2019-06-25 13:34:32 -0400

    changed some graphing

--- barium8fluorescence.py
+++ barium8fluorescence.py
@@ -29,20 +29,20 @@ Deltag = 0*sc.pi*0 #detuning of 493 laser
 Deltar = 0*sc.pi*0 #detuning of 650 laser
 #493 beams
 #Omg = 2*sc.pi*5 #Rabi frequrency of 2S1/2 to 2P1/2 
-Omgpi = 2*sc.pi*80 #Rabi frequrency of |1> to |3> and |2> to |4>
-Omgsp = 2*sc.pi*0 #Rabi frequrency of |1> to |4>
-Omgsm = 2*sc.pi*0 #Rabi frequrency of |2> to |3>
+Omgpi = 2*sc.pi*3.75 #Rabi frequrency of |1> to |3> and |2> to |4>
+Omgsp = 2*sc.pi*3.75 #Rabi frequrency of |1> to |4>
+Omgsm = 2*sc.pi*3.75 #Rabi frequrency of |2> to |3>
 #650 beams
 #Omr = 2*sc.pi*5 #Rabi frequrency of 2P1/2 to 2D3/2
-Omrpi = 2*sc.pi*10 #Rabi frequrency of |6> to |3> and |7> to |4>
-Omrsp = 2*sc.pi*50 #Rabi frequrency of |5> to |3> and |6> to |4>
-Omrsm = 2*sc.pi*50 #Rabi frequrency of |7> to |3> and |8> to |4>
+Omrpi = 2*sc.pi*5 #Rabi frequrency of |6> to |3> and |7> to |4>
+Omrsp = 2*sc.pi*5 #Rabi frequrency of |5> to |3> and |6> to |4>
+Omrsm = 2*sc.pi*5 #Rabi frequrency of |7> to |3> and |8> to |4>
 #detunings
 gammag =  2*sc.pi*15.1 #Decay rate of 2P1/2 to 2S1/2
 gammar =  2*sc.pi*5.3 #Decay rate of 2P1/2 to 2D3/2
-gammalg = 2*sc.pi*0.2 #493 laser linewidth
-gammalr = 2*sc.pi*0.2 #650 laser linewidth
-B = 60/10000 #B-field in Tesla
+gammalg = 2*sc.pi*0.0 #493 laser linewidth
+gammalr = 2*sc.pi*0.0 #650 laser linewidth
+B = 3.6/10000 #B-field in Tesla
 #tlist = np.linspace(0, 0.25, 10) #List of points for plotting purposes
 wB = ((sc.value('Bohr magneton')*B)/(sc.hbar))/1000000 #Larmor frequency in 2pi*MHz
 
@@ -168,26 +168,26 @@ fig = plt.figure()
 ax = fig.add_subplot(111)
 line1, = ax.plot(x, y, 'b-') 
 
-#Deltag = -2*sc.pi*200
-#while Deltag < 2*sc.pi*0:
-#    Deltar = -2*sc.pi*20 #detuning of 650 laser
-#    H = ((Deltag-wB)*sig11 + ((-2/R3)*Omgpi)*sig13 + (Deltag+wB)*sig22 + ((2/R3)*Omgpi)*sig24 + ((-2/R3)*Omgpi)*sig31 + (-wB/3)*sig33 + ((1j/R2)*Omrsp)*sig35 + ((2/R6)*Omrpi)*sig36 + ((-1j/R6)*Omrsm)*sig37 + ((2/R3)*Omgpi)*sig42 + (wB/3)*sig44 + ((1j/R6)*Omrsp)*sig46 + ((2/R6)*Omrpi)*sig47 + ((-1j/R2)*Omrsm)*sig48 + ((-1j/R2)*Omrsp)*sig53 + (Deltar-(6*wB/5))*sig55 + ((2/R6)*Omrpi)*sig63 + ((-1j/R6)*Omrsp)*sig64 + (Deltar-(2*wB/5))*sig66 + ((1j/R6)*Omrsm)*sig73 + ((2/R6)*Omrpi)*sig74 + (Deltar+(2*wB/5))*sig77 + ((1j/R2)*Omrsm)*sig84 + (Deltar+(6*wB/5))*sig88)
-#    final_state = steadystate(H, c_ops) #Solve Hamiltonian for t = infinity
-#    fexpt1 = expect(sig44, final_state)  #Gives expectation value of solved Hamiltonian for excited state
-#    fexpt2 = expect(sig33, final_state)
-#    FexptPtot = fexpt1 + fexpt2
-#    yg.append(FexptPtot)
-#    xg.append(Deltag/(2*sc.pi))
-#    Deltag += 2*sc.pi*1
-#
-#plt.plot(xg, yg,'c-')
-#plt.xlabel('Δg [MHz]')
-#plt.ylabel('Population in |3>+|4>')
-#plt.show()
+Deltag = -2*sc.pi*50
+while Deltag < 2*sc.pi*50:
+    Deltar = 2*sc.pi*20 #detuning of 650 laser
+    H = ((Deltag-wB)*sig11 + ((-2/R3)*Omgpi)*sig13 + (Deltag+wB)*sig22 + ((2/R3)*Omgpi)*sig24 + ((-2/R3)*Omgpi)*sig31 + (-wB/3)*sig33 + ((1j/R2)*Omrsp)*sig35 + ((2/R6)*Omrpi)*sig36 + ((-1j/R6)*Omrsm)*sig37 + ((2/R3)*Omgpi)*sig42 + (wB/3)*sig44 + ((1j/R6)*Omrsp)*sig46 + ((2/R6)*Omrpi)*sig47 + ((-1j/R2)*Omrsm)*sig48 + ((-1j/R2)*Omrsp)*sig53 + (Deltar-(6*wB/5))*sig55 + ((2/R6)*Omrpi)*sig63 + ((-1j/R6)*Omrsp)*sig64 + (Deltar-(2*wB/5))*sig66 + ((1j/R6)*Omrsm)*sig73 + ((2/R6)*Omrpi)*sig74 + (Deltar+(2*wB/5))*sig77 + ((1j/R2)*Omrsm)*sig84 + (Deltar+(6*wB/5))*sig88)
+    final_state = steadystate(H, c_ops) #Solve Hamiltonian for t = infinity
+    fexpt1 = expect(sig44, final_state)  #Gives expectation value of solved Hamiltonian for excited state
+    fexpt2 = expect(sig33, final_state)
+    FexptPtot = fexpt1 + fexpt2
+    yg.append(FexptPtot)
+    xg.append(Deltag/(2*sc.pi))
+    Deltag += 2*sc.pi*1
 
-Deltar =  -2*sc.pi*200
-while Deltar < 2*sc.pi*250:
-    Deltag =  -2*sc.pi*115
+plt.plot(xg, yg,'c-')
+plt.xlabel('Δg [MHz]')
+plt.ylabel('Population in |3>+|4>')
+plt.show()
+
+Deltar =  -2*sc.pi*60
+while Deltar < 2*sc.pi*40:
+    Deltag =  -2*sc.pi*10
     H = ((Deltag-wB)*sig11 + ((-2/R3)*Omgpi)*sig13 + (Deltag+wB)*sig22 + ((2/R3)*Omgpi)*sig24 + ((-2/R3)*Omgpi)*sig31 + (-wB/3)*sig33 + ((1j/R2)*Omrsp)*sig35 + ((2/R6)*Omrpi)*sig36 + ((-1j/R6)*Omrsm)*sig37 + ((2/R3)*Omgpi)*sig42 + (wB/3)*sig44 + ((1j/R6)*Omrsp)*sig46 + ((2/R6)*Omrpi)*sig47 + ((-1j/R2)*Omrsm)*sig48 + ((-1j/R2)*Omrsp)*sig53 + (Deltar-(6*wB/5))*sig55 + ((2/R6)*Omrpi)*sig63 + ((-1j/R6)*Omrsp)*sig64 + (Deltar-(2*wB/5))*sig66 + ((1j/R6)*Omrsm)*sig73 + ((2/R6)*Omrpi)*sig74 + (Deltar+(2*wB/5))*sig77 + ((1j/R2)*Omrsm)*sig84 + (Deltar+(6*wB/5))*sig88)
     final_state = steadystate(H, c_ops) #Solve Hamiltonian for t = infinity
     fexpt1 = expect(sig44, final_state)  #Gives expectation value of solved Hamiltonian for excited state
@@ -203,15 +203,16 @@ plt.xlabel('Δr [MHz]')
 plt.ylabel('Population in |3>+|4>')
 plt.show()
 
-thefile = open('G:\\Team Drives\\Ions\\03 - Projects\\Current Projects\\Rb Ba+ hybrid\\493 Photon Shape Tests\\DeltaR_8Mar.dat', 'w')
-for item in xr:
-  thefile.write("%s\n" % item)
-thefile.close()
+print(wB)
+##thefile = open('G:\\Team Drives\\Ions\\03 - Projects\\Current Projects\\Rb Ba+ hybrid\\493 Photon Shape Tests\\DeltaR_8Mar.dat', 'w')
+##for item in xr:
+##  thefile.write("%s\n" % item)
+##thefile.close()
 
-thefile = open('G:\\Team Drives\\Ions\\03 - Projects\\Current Projects\\Rb Ba+ hybrid\\493 Photon Shape Tests\\PopPR_8Mar.dat', 'w')
-for item in yr:
-  thefile.write("%s\n" % item)
-thefile.close()
+##thefile = open('G:\\Team Drives\\Ions\\03 - Projects\\Current Projects\\Rb Ba+ hybrid\\493 Photon Shape Tests\\PopPR_8Mar.dat', 'w')
+##for item in yr:
+##  thefile.write("%s\n" % item)
+##thefile.close()
 ###
 #thefile = open('G:\\Team Drives\\Ions\\03 - Projects\\Current Projects\\Rb Ba+ hybrid\\493 Photon Shape Tests\\DeltaR493-8Mar.dat', 'w')
 #for item in xg:
